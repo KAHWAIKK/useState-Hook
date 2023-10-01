4926f5bced12762152413b22572cdc67b4809fde
KAHWAIKK <kahwaikagwima@gmail.com>
2023-10-01 17:50:06 +0300

    Added a re-sable component via prop drilling

--- src/LineItem.jsx
+++ src/LineItem.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { FaTrashAlt } from "react-icons/fa"
 
-const LineItem = () => {
+const LineItem = ({item,handleCheck,handleDelete}) => {
   return (
-    <div>
-        
-    </div>
+    <li className="item" key={item.id}>
+            <input 
+            type="checkbox"
+            onChange={() => handleCheck(item.id)}
+            checked = {item.checked} 
+            />
+            <label 
+            style={(item.checked) ? {'textDecoration': 'line-through'}: null}
+            onDoubleClick={() => handleCheck(item.id)}
+            >{item.item}
+            </label>
+            <FaTrashAlt
+            onClick={() => handleDelete(item.id)} 
+            role="button" 
+            tabIndex="0"
+            aria-label={`Delete ${item.id}`}//add aria-label for accessibility purposes
+            />
+    </li>
   )
 }
 
